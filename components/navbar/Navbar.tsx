2f2af4296dbbe0b5db058a2d75b6bede07005d61
umidjon11111 <umidjongafforov844@gmail.com>
2025-12-16 19:32:14 +0500

    fix

---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <a href="/">Home </a>
+      <a href="/about">About </a>
+    </div>
+  );
+};
+
+export default Navbar;
